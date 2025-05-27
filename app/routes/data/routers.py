166b9843a402de472502e9466659c31a58999327
C418____11 <553515788@qq.com>
2025-05-27 10:34:41 +0800

    实现create_row和delete_row

--- app/routes/data/routers.py
+++ app/routes/data/routers.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 
 
-from flask import Blueprint
-from flask_jwt_extended import jwt_required
+import ast
+import re
 
+from flask import Blueprint
+from flask import request
+from flask_jwt_extended import jwt_required
+from sqlalchemy.exc import IntegrityError
+
+from ...api import APIArgumentError
 from ...api import DataTableNotFound
 from ...api import GetRows
 from ...api import GetTables
 from ...api import RequestSuccess
 from ...api import api
+from ...extensions import db
 from ...model_utils import BaseModel
 from ...model_utils.utils import ColumnInfo
 from ...models.data import EDITABLE_TABLE_NAMES
@@ -19,7 +26,6 @@ bp = Blueprint("data", __name__)
 
 COLUMN_INFO: dict[str, dict[str, ColumnInfo]] = BaseModel.get_columns_info()  # type: ignore[assignment]
 NAME2TABLE: dict[str, type[BaseModel]] = BaseModel.name2table()  # type: ignore[assignment]
-
 
 LIMIT_VISIBILITY = False
 
@@ -58,10 +64,60 @@ def get_rows(table_name: str, offset: int, limit: int) -> GetRows | DataTableNot
 @jwt_required()  # type: ignore[misc]
 @api
 @permissions_required([PERMISSIONS.DATA.CREATE])
-def create_row(table_name: str) -> RequestSuccess | DataTableNotFound:
+def create_row(table_name: str) -> RequestSuccess | DataTableNotFound | APIArgumentError:
     if LIMIT_VISIBILITY and table_name not in EDITABLE_TABLE_NAMES:
         return DataTableNotFound()
-    # table = NAME2TABLE[table_name]  # todo implement
+
+    table = NAME2TABLE[table_name]
+
+    if not isinstance(request.json, dict):
+        return APIArgumentError(arguments={"_schema": ["Invalid input type."]})
+
+    try:
+        obj = table(**{k: request.json[k] for k in request.json.keys() - {"id"}})
+    except TypeError as err:
+        if match := re.match(r"'(.*)' is an invalid keyword argument for ", str(err)):
+            return APIArgumentError(arguments={match.group(1): ["invalid argument"]})
+        raise
+
+    db.session.add(obj)
+
+    try:
+        db.session.commit()
+    except IntegrityError as err:
+        db.session.rollback()
+        if match := re.search(r"UNIQUE constraint failed: [^.]*\.(.*)", str(err)):
+            params: str = re.search(r"\[parameters: (.*)]", str(err)).group(1)
+            params_obj: tuple[str, ...] = ast.literal_eval(params)
+            return APIArgumentError(
+                arguments={match.group(1): [f"unique constraint failed: {param}"] for param in params_obj}
+            )
+        raise
+    except Exception:
+        db.session.rollback()
+        raise
+    return RequestSuccess()
+
+
+@bp.route("/tables/<string:table_name>/rows/<int:row_id>", methods=["DELETE"])
+@jwt_required()
+@api
+@permissions_required([PERMISSIONS.DATA.DELETE])
+def delete_row(table_name: str, row_id: int) -> RequestSuccess | DataTableNotFound:
+    if LIMIT_VISIBILITY and table_name not in EDITABLE_TABLE_NAMES:
+        return DataTableNotFound()
+
+    table = NAME2TABLE[table_name]
+    obj = table.query.get(row_id)
+    if obj is None:
+        return DataTableNotFound()
+
+    db.session.delete(obj)
+    try:
+        db.session.commit()
+    except Exception:
+        db.session.rollback()
+        raise
     return RequestSuccess()
 
 
